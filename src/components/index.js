8c24f83dfed17a85e0296fd2c48242d466c55676
Hhjalmar <56554099+Hhjalmar@users.noreply.github.com>
2021-11-14 15:49:04 +0200

    ain

--- src/components/index.js
+++ src/components/index.js
@@ -76,7 +76,6 @@ export { default as LayoutWrapperTopbar } from './LayoutWrapperTopbar/LayoutWrap
 export { default as LayoutSideNavigation } from './LayoutSideNavigation/LayoutSideNavigation';
 export { default as LayoutSingleColumn } from './LayoutSingleColumn/LayoutSingleColumn';
 
-
 // Menu
 export { default as MenuItem } from './MenuItem/MenuItem';
 export { default as MenuContent } from './MenuContent/MenuContent';
