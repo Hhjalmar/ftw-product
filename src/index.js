d5f35a10031e727046cc766bdb6a7db70a02ccc3
Hhjalmar <56554099+Hhjalmar@users.noreply.github.com>
2021-11-19 09:22:12 +0200

    main

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import 'core-js/features/map';
 import 'core-js/features/set';
 import 'raf/polyfill';
 
-import Bugsnag from '@bugsnag/js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { loadableReady } from '@loadable/component';
@@ -36,10 +36,7 @@ import { LoggingAnalyticsHandler, GoogleAnalyticsHandler } from './analytics/han
 
 import './styles/marketplaceDefaults.css';
 
-Bugsnag.start({
-  apiKey: '73127232b3e6098a4639f4d1291ca639',
-  plugins: [new BugsnagPluginReact(React)]
-})
+
 
 const render = (store, shouldHydrate) => {
   // If the server already loaded the auth information, render the app
