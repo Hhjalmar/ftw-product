966e3943757c2bebf7327439935df540402ab8e4
Hjalmar <56554099+Hhjalmar@users.noreply.github.com>
2022-08-11 10:46:15 +0300

    main

--- src/containers/BlogPage/BlogPage.js
+++ src/containers/BlogPage/BlogPage.js
@@ -10,6 +10,9 @@ import {
 
 import StaticPage from '../../containers/StaticPage/StaticPage';
 import TopbarContainer from '../../containers/TopbarContainer/TopbarContainer';
+import image from './Alvar_Aalto_in_his_Studio_1945_web-1536x864.jpeg';
+
+
 
 import css from './BlogPage.module.css';
 
@@ -26,167 +29,37 @@ const BlogPage = () => {
           <h1>BLOG</h1>
 
           <div>
-          <h3>Why beautiful things make us happy</h3>
-
-            <p>A lot of things can be beautiful.
-
-Landscapes, faces, fine art, or epic architecture; stars in the sky.
-
-Or simply the reflection of the sun on an empty bottle.
-
-Beauty is nothing tangible, it only exists in our heads as a pleasant feeling.
-
-If we have to define it, we perceive something as beautiful if its color, shape, form, or proportion
-
-somehow are appealing or delightful to us.
-
-Beauty is a very human experience that's been with us for millions of years.
-
-Even our first tools were trimmed to a symmetrical shape.
-
-Researchers have tried to find practical reasons why our ancestors invested the time to make their tools look nice, but couldn't really identify any.
-
-It seems that early humans shaped their tools into teardrops, simply because they liked them better that way.
-
-Throughout our history, the definition of beauty has changed a lot.
-
-Ideals have shifted or turned into their opposites.
-
-But beyond individual and contemporary tastes some things have never really gone out of fashion.
-
-The golden ratio, symmetry, or fractal patterns can be found in the art and architecture of cultures from our beginnings, to today.
-
-Humans seem to be in mysterious, inherent agreement about the beauty of certain things.
-
-The patterns that keep coming up are all rooted in nature.
-
-They became part of our biology because they helped our ancestors survive.
-
-Fractal patterns for example, occur all over nature.
-
-In snail shells, flower heads; waves or clouds
-
-Identifying and assessing these things and phenomena correctly used to be vital.
-
-Do those clouds mean rain will come soon? Are these waters safe to swim through? Can I eat this?
-
-Another pervasive thing is symmetry.
-
-In nature it means everything is as it should be
-
-Stems and trees and leaves and blossoms all grow symmetrically
-
-A deer with impressive antlers is probably a source of nutritious meat.
-
-A deformed wheat hair may not be safe to eat.
-
-A symmetrical face is more likely to belong to a healthy and fertile mating partner.
-
-Because symmetry is so common in fauna and flora, it's extremely familiar to our brain.
-
-It helped our ancestors evaluate their environment more easily, and react quickly to danger.
-
-Things that helped us survive activate the reward center in our brain.
-
-recognising signals of safety and nutrition, triggered nice feelings in us.
-
-So our sense of beauty probably evolved from pattern recognition, but it goes way beyond that now.
-
-Humans seem to have evolved an instinct for beauty that is deeply hardwired into us.
-
-It remain even after other processes in our brain stop working.
-
-Alzheimer's patients were asked to rank the beauty of several paintings
-
-Then the experiment was repeated two weeks later
-
-The patients have long since forgotten the paintings, but still ranked the beauty of the paintings in the same order.
-
-One could argue that this doesn't say much. So what if people stick to their personal preferences?
-
-But other research has shown that we have a sort of lowest common denominator when it comes to beauty.
-
-In different experiments, people were asked to distinguish real from fake abstract paintings.
-
-Some were originals by Mondrian and Pollock that were painted based on strict rules like fractal patterns, while the imitations were not.
-
-The majority picked out the original artworks.
-
-This worked for paintings from both artists, even though their arts are very different.
-
-Another experiment also used abstract artworks but, asked people to pick them out among similar paintings made either by children or animals.
-
-Again, the test subjects pointed out the legit paintings whose patterns were carefully planned and not random
-
-So while we have a hard time pinning down what beauty is or what it's based on, we somehow recognize it when we see it
-
-Humans don't navigate nature trying to survive day by day anymore
-
-We left the natural world behind and created our own.
-
-We made the objects that surround us the things we wear and use and look at.
-
-As we spread over the planet and our numbers grew, we shaped a completely man-made environment.
-
-In the process of doing so, we often neglected beauty in favor of functionality cost or efficiency.
-
-We built rows and rows of concrete housing blocks that nobody wants to live in.
-
-We have ugly underground subway stations, shabby public service buildings, and sprawling malls.
-
-One bland, standardized box beside the next.
-
-Humans, don't like monotony.
-
-Eye tracking software has shown that people keep focusing on details and ornaments of architecture while brushing quickly over blank walls
-
-And not only are they no fun to look at, they actually make us miserable.
-
-Experiments with skin sensors showed that looking at vast, dull facades makes us feel bored and uncomfortable.
-
-This kind of boredom has been linked to raised heart rates and stress levels and the opposite seems to be true, too.
-
-Over the last decades more and more studies have found that surroundings that are actually aesthetically pleasing to us can improve our well-being, our behavior,
-
-cognitive function, and mood.
-
-Our bodies and brains react measurably and visibly to everything that surrounds us
-
-Beauty in particular has such a strong impact on our well-being that making useful things beautiful can actually make them better.
-
-In 2017, a hospital examined recovery factors through observation and interviews with patients and found that visual art in their lounge areas
-
-made them more comfortable and happier about their stay in general.
-
-Another study looked at how well patients recovered in a hospital that had two wards.
-
-A very old and rather ugly one, and a newly renovated ward.
-
-To the researchers' surprise, the patients that stayed in the new, renovated environment, needed less pain medication
-
-and were released on average, two days earlier than patients recovering in the old ward.
-
-More beautiful surroundings made them feel better, physically.
-
-Beauty also has an effect on us on a daily basis.
-
-It can improve our general happiness.
-
-A study that looked at the main factors influencing the happiness of adults revealed an unexpected result.
-
-Besides things like good health in a harmonious family life, individual happiness is affected by how beautiful you find the city you live in.
-
-Beauty scored even higher than cleanliness or safety.
-
-So what can we learn from all of this?
-
-We know that we humans have been fine-tuned for millions of years to process visual input and assess our surroundings.
-
-It's just what we're programmed to do and we're starting to learn more about how much beauty as a property is really influencing us.
-
-Beauty meets an inherent need for meaningful information.
-
-Maybe it would be worth giving it more space in this man-made world we have created.</p>
+          <h3>Who was Alvar Aalto?</h3>
+          <img className={css.coverImage} src={image} alt="Alvar_Aalto_in_his_Studio_1945_web-1536x864.jpeg" align="center" width="800" height="500"  />
+
+            <p> Alvar Aalto, Architect and Artist – that was the name of the first studio which Finnish architect and designer Alvar Aalto opened in 1923, while he was still studying at Helsinki University of Technology. Before that he already had an experience of designing his parents’ entire house. Since then his professional, successful career growth started. 
+Soon his first wife, Aino Marsio, also an architect, joined him, so they developed business together. At the same time, they designed their new house in Helsinki, the Villa Aalto. House was designed to be their office as well. Later, during the 1950’s, due to the increased amount of work, Alvar designed separate studio with his second wife, architect Elissa Mäkiniemi.  He named it simply Studio Aalto. Today Villa Aalto is a museum, while the Studio Aalto is in hands of the Alvar Aalto Academy. 
+The Pioneer of Nordic Modernism</p>
+<p>Among his first architectural works Alvar Aalto included numerous private projects as well, such as family houses and summer houses, but he also successfully worked on functional solutions for business clients. First works, in accordance with his traditional education, were classical. They belonged to Nordic Classicism inspired by national romanticism heritage. 
+Besides projects for his family, cousins and friends, he was responsible for several public facilities at the time, such as Jyväskylä Workers’ Club (1925), Jyväskylä Defence Corps building (1926) and Seinäjoki Defence Corp building (1924–1929).
+However, Aalto soon turned to more modern design and became one of the pioneers of Nordic Modernism together with architects and designers Arne Jacobsen from Denmark and Arne Korsmo from Norway.
+His work became more expressive and more conceptual thus. His Villa Mairea from 1939 was an example of such a design. Significant projects followed, like Baker House within Massachusetts Institute of Technology in the USA (1946-49), and then Säynätsalo Town Hall (1952), Rautatalo office building (1955), Vuoksenniska Church (1959) and Finlandia Hall (1973) in Finland. 
+For his contribution to architecture he received numerous awards, among which were Prince Eugen Medal in 1954, RIBA Gold Medal in1957, and AIA Gold Medal in 1963. 
+Art Inspired by Nature</p>
+<p>During his multi-decade long career Aalto projected more than 200 buildings, but was equally successful as furniture and interior objects designer. Vast number of items that he designed became symbols of Scandinavian modernism and are still in production.
+Alvar Aalto found the inspiration in nature and his own surrounding and he was prone to using natural materials. He used to say that every piece of furniture which is directly in contact with human body simply had to be made of natural material. That was one thing he couldn’t compromise with. 
+He was excellent with any material that he worked with, whether it was wood, metal, glass or fabric. However he was especially remembered for experimenting with pressing and bending of laminate. Using this technique his items got original, twisted and sculptural shapes. 
+“It’s not an art to take and copy everything from tradition or past, it’s necessary to take the material and energy from nature and respond with the work of art, bringing your own psychical energy into it. We are prone to take everything from nature without giving anything in return. That’s not good – it can take a revenge on us.” Alvar Aalto said, as stated in a book Design and designers of the 20th century, written by Radmila Milosavljevic and Marijana Milosavljevic.  
+That’s why he, as a contribution to the beauties of nature and inspired by water waves, designed one of the most famous vases in history. It was the glass, wavy Savoy Vase. He designed it together with his wife Aino, for the occasion of opening the Savoy restaurant in Helsinki. The vase delighted everyone. Decorators regarded it as very inspirational, so much that they exposed it as a decoration. On some other instances several vases were put together along their wavy lines, while others were placed with flower bouquets. 
+Although it was in 1937, the vase is still one of the most impressive examples of Alvar Aalto’s authentic style. Finnish glass factory Karhula-Littala that produced the Savoy Vase even won the award at the World Exhibition in Paris in 1937, within the International glass exhibition. It was often simply called Aalto Vase and it’s interesting that the word “aalto” in Finnish means “wave” which in this case has multiple symbolism.
+The Savoy Vase was later produced in whole spectrum of colors, in different sizes and shapes. It is still produced according to the original Alvar’s design by the same glass factory – Littala from the Finnish town of Littala. 
+Between humanism and materialism</p>
+
+<p>As for the furniture design, Alvar Aalto’s approach was under great influence of Bauhaus artists and designers, marked with functionality to a large degree. His solutions were characterized mostly by intuitive design and synthesis of playful and pragmatic ideas. After his honeymoon in Italy, and later Mediterranean journeys, the influence of Renaissance in his work increased.
+He liked to use unusual forms, play with sound and light and experiment with materials, but always making the final product functional above all. 
+Rational approach to architecture is best seen in his projects for Paimio Sanatorium which was built from 1929 until 1933, as well as Viipuri City Library from 1927 until 1935.  He also designed the interior and all furniture for his architectural solutions, connecting practical and aesthetic side but leaving a personal touch as well.  
+That’s how Paimio Chair was made, one of the most famous chairs of Alvar Aalto. The wavy lines of its armrest remind of waviness seen in the Savoy Vase. The seat was made of molded plywood and was skillfully fitted into the frame so it seems like the chair is floating. 
+Chairs, armchairs, tables, lamps and other furniture designed by Alvar Aalto have been produced by the Artek Company since 1935. Alvar and Aino Aalto, together with their patron Maire Gullichsen and art historian Nils-Gustav Hahl, founded the Company with a wish to distribute furniture and promote modern living culture through exhibitions and educative programs. Artek is still doing business successfully in Helsinki and Tokyo and is leading a separate platform for furniture restoration.
+Alvar Aalto was not only one of the most recognized architects and designers in Europe, but was also extremely appreciated in America. New York Museum of Modern Art (MoMA) organized several exhibitions in his honor. The first one under the name Alvar Aalto: Architecture and Furniture was presented in 1938. In 1984, MoMA organized an exhibition dedicated to his furniture design and glass work –  Alvar Aalto: Furniture and Glass. On centenary of his birth in 1998, a huge retrospective was arranged – Alvar Aalto: Between Humanism and Materialism including original drawings, sketches and architectural models display. </p>
+<p>Alvar Aalto – the giant of Finnish nation
+With his extreme devotion to urbanism and rational planning, Alvar Aalto left a deep mark on Finnish and Scandinavian architectural scene. The fact that he is considered to be one of the nation’s giants supports this. That’s how Aalto University in Helsinki, founded in 2010 by merging Helsinki University of Technology, Helsinki School of Economics and University of Art and Design Helsinki, got its name in the honor of his contribution to national architecture and urban culture. 
+The University’s seat, designed by Alvar Aalto himself, is within the campus in the town of Otaniemi. It consists of six schools with 17,500 students.</p>
+<p>On their own initiative, students started the Aalto Design Factory, Aalto Ventures Program and Aalto Entrepreneurship Society (also known as Aaltoes) with the goal to increase the support to students with entrepreneurial ideas who wanted to develop companies of their own. It resulted in numerous innovative solutions and only in few years made Finland become one of the countries with the most developed start-up system in the world. Aalto University set the foundation for experimental education system in a way. It proved to be very successful connecting science, art and business society in Finland and making breeding ground for modern education, multidisciplinary research and progressive development. These are the exact principles on which Alvar Aalto based his whole career.</p>
         
 
            
